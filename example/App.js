c32b3e0dc7494935c970d11a3f10b5c4c065f031
zusinShinpei <kmh011225@naver.com>
2020-01-17 23:24:24 +0900

    Typo

--- example/App.js
+++ example/App.js
@@ -6,106 +6,55 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
 
-import KakaoSDK from 'react-native-ccs-kakaosdk';
+import KakaoSDK from "@actbase/react-native-kakaosdk";
 
-type Props = {};
-export default class App extends Component<Props> {
+export default function App() {
+  const addFriend = id => {
+    KakaoSDK.channel.addFriend(id);
+  };
 
-	state = {
+  const chat = id => {
+    KakaoSDK.channel.chat(id);
+  };
 
-	};
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => addFriend("_asdfgh")}
+        style={styles.button}
+      >
+        <Text>AddFriend</Text>
+      </TouchableOpacity>
 
-	constructor(props) {
-		super(props);
-	}
-
-	login() {
-		this.setState({ loading: true });
-
-		KakaoSDK.login().then((response) => {
-			console.log(response);
-			this.setState({ loading: false });
-		}).catch(e => {
-			console.log(e);
-			this.setState({ loading: false });
-		});
-	}
-
-	async logout() {
-		this.setState({ loading: true });
-
-		KakaoSDK.logout().then((response) => {
-			console.log(response);
-			this.setState({ loading: false });
-		}).catch(e => {
-			console.log(e);
-			this.setState({ loading: false });
-		});
-	}
-
-	async getToken() {
-		this.setState({ loading: true });
-
-		KakaoSDK.getAccessToken().then(token => {
-			alert(JSON.stringify(token));
-			this.setState({ loading: false });
-		}).catch(e => {
-			console.log(e);
-			this.setState({ loading: false });
-
-		});
-	}
-		
-	render() {
-		return (
-			<View style={styles.container}>
-
-				{(this.state.loading && true) && (
-				<View style={ styles.loading }>
-					<ActivityIndicator />
-				</View>
-				)}
-
-				<TouchableOpacity onPress={() => this.login() } style={ styles.button }>
-					<Text>Login</Text>
-				</TouchableOpacity>
-
-				<TouchableOpacity onPress={() => this.logout() } style={ styles.button }>
-					<Text>Logout</Text>
-				</TouchableOpacity>
-
-				<TouchableOpacity onPress={() => this.getToken() } style={ styles.button }>
-					<Text>getToken</Text>
-				</TouchableOpacity>
-			</View>
-		);
-	}
+      <TouchableOpacity onPress={() => chat("_asdfgh")} style={styles.button}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-
-	loading : {
-		...StyleSheet.absoluteFill, 
-		zIndex: 10, 
-		backgroundColor: 'rgba(0,0,0,0.8)', 
-		alignItems: 'center', 
-		justifyContent: 'center'
-	},
-
-	button: {
-		borderColor: '#000', 
-		borderWidth: 1, 
-		padding: 10, 
-		width: 200, 
-		margin: 10,
-	}
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  button: {
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+    margin: 10
+  }
 });
--- index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "react-native-kakao-channel" {
-  interface ARNKakaoChannel {
-    addFriend: (id: string) => Promise<"SUCCESS">;
-    chat: (id: string) => Promise<"SUCCESS">;
-  }
-
-  export default ARNKakaoChannel;
-}
