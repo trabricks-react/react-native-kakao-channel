2e8daa59a34e643482d6d6b527cf461928b762b2
zusinShinpei <react-native@kakao.com>
2021-01-30 17:03:16 +0900

    Fix Build Error

--- ios/ARNKakaoChannel.swift
+++ ios/ARNKakaoChannel.swift
@@ -9,18 +9,19 @@
 import Foundation
 import KakaoSDKTalk
 import KakaoSDKCommon
+import WebKit
 
 @objc(ARNKakaoChannel)
 public class ARNKakaoChannel: NSObject {
     private var safariViewController: SFSafariViewController?
     
     public override init() {
-        var appKey: String = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as String
-        KakaoSDKCommon.initSDK(appKey: appKey)
+        var appKey: String? = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as String?
+        KakaoSDKCommon.initSDK(appKey: appKey!)
     }
     
-    func presentSafari(url: String,
-                       completion: (Error?) -> void) -> Bool {
+    func presentSafari(url: URL,
+                       completion: (Error?) -> Void) -> Bool {
         self.safariViewController = SFSafariViewController(url: url)
         self.safariViewController?.modalTransitionStyle = .crossDissolve
         self.safariViewController?.modalPresentationStyle = .overCurrentContext
@@ -29,12 +30,12 @@ public class ARNKakaoChannel: NSObject {
         }
     }
     
-    @objc(addFriend:friendId:resolve:reject:)
+    @objc(addFriend:resolve:reject:)
     func addFriend(_ friendId: NSString,
                    resolver resolve: @escaping RCTPromiseResolveBlock,
                    rejector reject: @escaping RCTPromiseRejectBlock) -> Bool {
-        var url: String = TalkApi.shared.makeUrlForAddChannel(channelPublicId: friendId)
-        self.presentSafari(url: url, (error) {
+        var url: URL? = TalkApi.shared.makeUrlForAddChannel(channelPublicId: friendId as String)
+        self.presentSafari(url: url!, (error) {
             if (error) {
                 reject(error);
             } else {
@@ -43,12 +44,12 @@ public class ARNKakaoChannel: NSObject {
         })
     }
     
-    @objc(chat:friendId:resolve:reject:)
+    @objc(chat:resolve:reject:)
     func chat(_ friendId: NSString,
               resolver resolve: @escaping RCTPromiseResolveBlock,
               rejector reject: @escaping RCTPromiseRejectBlock) -> Bool {
-        var url: String = TalkApi.shared.makeUrlForChannelChat(channelPublicId: friendId)
-        self.presentSafari(url: url, (error) {
+        var url: URL? = TalkApi.shared.makeUrlForChannelChat(channelPublicId: friendId as String)
+        self.presentSafari(url: url!, (error) {
             if (error) {
                 reject(error);
             } else {
